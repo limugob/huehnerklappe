8b25055f19bfe9202f4accb20292a119223797d8
limubog <it@schumik.de>
2017-02-20 01:13:24 +0100

    play some music before close

--- door.py
+++ door.py
@@ -3,9 +3,11 @@
 
 import os
 import requests
+import subprocess
 
 USER = ''
 PASS = ''
+PATH = ''
 
 class Door:
     # if STATE_FILE exists, the door is closed.
@@ -22,6 +24,14 @@ class Door:
         requests.post('https://limugob.pythonanywhere.com/log', data={'message': 'DOOR_OPEN'}, auth=(USER,PASS))
 
     def close(self):
+        self.pre_close_hook()
         file = open(self.state_file, 'w')
         file.close()
         requests.post('https://limugob.pythonanywhere.com/log', data={'message': 'DOOR_CLOSE'}, auth=(USER,PASS))
+
+    def pre_close_hook(self):
+        # play some music before close
+        subprocess.Popen(['mpg123', '-q', PATH]).wait()
+
+
+
